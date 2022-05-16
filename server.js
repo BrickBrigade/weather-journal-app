35f5bf9a01a64212a94b7f12292c911af94aba65
BrickBrigade <101427661+BrickBrigade@users.noreply.github.com>
2022-05-16 13:01:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,27 +6,34 @@ const express = require('express');
 
 // Start up an instance of app
 const app = express();
-/* Middleware*/
+
+/* Dependencies */
 const bodyParser = require('body-parser');
-//Here we are configuring express to use body-parser as middle-ware.
+/* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
-app.use(cors);
+app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
-// Setup Server
 const port = 3000;
+// Setup Server
 const server = app.listen(port, listening);
-function listening(){
-	console.log('Server running...');
-	console.log(`Running on port: ${port}.`);
+ function listening(){
+	console.log(`running on localhost: ${port}`);
 }
 
-app.get('/', (req, res) => {
-	res.send('hello')
-})
+app.get('/all', sendData);
+function sendData (req, res) {
+	res.send(projectData);
+};
+
+// POST an entry
+app.post('/add', addEntry);
+function addEntry (req, res){
+	projectData.push(req.body);
+};
